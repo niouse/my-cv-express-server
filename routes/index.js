743a6adf4efd194f143284cb5ce0bba0cdbbbe9c
Niouse <seba.pinard@gmail.com>
2018-07-04 23:58:53 +0200

    change message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/api/test', function(req, res, next) {
-  res.send({ title: 'hello ca va ' });
+  res.send({ title: 'hello ca va bien là haut ?' });
 });
 router.get('*', function(req, res, next) {
   res.send('index.html');
