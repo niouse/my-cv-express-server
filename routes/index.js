d73976d8da81f0da732384d1eef8f58bcd61fb29
Niouse <seba.pinard@gmail.com>
2018-07-05 00:05:12 +0200

    lint

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/api/test', function(req, res, next) {
+router.get('/api/test', function(req, res) {
   res.send({ title: 'hello ca va bien là haut ?' });
 });
-router.get('*', function(req, res, next) {
+router.get('*', function(req, res) {
   res.send('index.html');
 });
+
+// router.get('*', function(req, res, next) {
+//   res.send('index.html');
+// });
 
 module.exports = router;
