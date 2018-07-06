1f6fa03ffe21ba1d81075e0558101a28aa4be69e
Niouse <seba.pinard@gmail.com>
2018-07-07 00:23:07 +0200

    create routes and data files

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-const bley = 'tr'
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs')
+var path = require('path');
 
 /* GET home page. */
-router.get('/api/test', function(req, res) {
+router.get('/api/test', function (req, res) {
   res.send({ title: 'hello ca va bien là haut ?' });
 });
-router.get('*', function(req, res) {
-  res.send('index.html');
+router.get('/cv', function (req, res) {
+  console.log(res)
+  res.render('index.html');
+});
+
+router.get('/cv/:type/:lng?', function (req, res) {
+  var lng = req.params.lng || "fr"
+  var type = req.params.type
+  var uri = path.join(__dirname, `./../data/${lng}/${type}.json`)
+  fs.readFile(uri, 'utf8', function (error, data) {
+    if (error) {
+      res.send(new Error())
+    }
+    else {
+      res.send(JSON.parse(data))
+    }
+  })
 });
 
 // router.get('*', function(req, res, next) {
